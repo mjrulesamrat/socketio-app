03017340ffc120044f7ceddf584810c5f76abe9a
Jay Modi <mjrulesamrat@gmail.com>
2021-03-20 23:48:32 +0530

    Socket.io example connect/disconnect

---
+++ README.md
@@ -0,0 +1,13 @@
+# Socket.io Example App
+
+- install dependencies with below command
+
+    ```npm install```
+
+- run app wih
+
+    ```npm start```
+
+- Go to home page and check console logs
+
+    ```localhost:3000```
---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
