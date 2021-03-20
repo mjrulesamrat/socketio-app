bb8e9633771afd9670c3f7e0372e425e08854847
Jay Modi <mjrulesamrat@gmail.com>
2021-03-21 00:04:40 +0530

    Emit and show message

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
+    socket.on('chat message', (msg) => {
+        console.log(`message ${msg}`)
+        io.emit('chat message', msg);
+    })
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
